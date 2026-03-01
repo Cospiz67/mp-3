a24a5644ac153d54f557397ca5d1bda21f42aace
Cospiz67 <jeanne.durand@epita.fr>
2026-03-01 17:46:51 -0500

    created main

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Header from './components/Header.tsx';
 import Footer from './components/Footer.tsx';
 import {NavMainWrapper} from './components/NavMainWrapper.tsx';
 import Nav from './components/Nav.tsx';
+import Main from './components/Main.tsx'; 
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <Header/>
         <NavMainWrapper>
           <Nav/>
-          
+          <Main/>
         </NavMainWrapper>
         <Footer/>
       </Wrapper>
---
+++ src/components/Main.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const StyledMain = styled.main`
+    min-height: 100vh;
+    width:100%;
+`
+
+export default function Main(){
+    return(
+        <StyledMain>
+            
+        </StyledMain>
+    )
+}
